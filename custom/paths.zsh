8cc8845d0f6830a00d1c28ccbdb55ac422b4778f
Thorsten Lorenz <thlorenz@gmx.de>
2012-12-02 16:34:59 -0500

    adding more paths

--- custom/paths.zsh
+++ custom/paths.zsh
@@ -1,4 +1,6 @@
-export JS_PROJECTS="$HOME/dev/js/projects"
+hash -d js-pro="$HOME/dev/js/projects"
+hash -d bjarne="$HOME/dev/c/tuts/C++-Bjarne"
+
 
 # Landscape
 export NODE_PATH="/Users/thlorenz/dev/lab49/Landscape/trunk/packages"
