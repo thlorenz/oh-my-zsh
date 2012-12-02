048af3d07771a81ce1fe7b6f98bd9744e6c24258
Thorsten Lorenz <thlorenz@gmx.de>
2012-12-02 13:59:16 -0500

    using build in vicmd mode indicator

--- custom/misc.zsh
+++ custom/misc.zsh
@@ -4,16 +4,6 @@ export EDITOR=vim
 # dircolors 
 export CLICOLOR=1
 
-# Adapt right side prompt to reflect if we are in normal or insert mode
-function zle-line-init zle-keymap-select {
-    RPS1="${${KEYMAP/vicmd/▌}/(main|viins)/_}"
-    RPS2=$RPS1
-    zle reset-prompt
-}
-
-zle -N zle-line-init
-zle -N zle-keymap-select
-
 # Landscape
 
 export NODE_PATH="/Users/thlorenz/dev/lab49/Landscape/trunk/packages"
