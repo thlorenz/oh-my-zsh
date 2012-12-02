522ccdb05ed92a83b2a4439279647d13415ab76e
Thorsten Lorenz <thlorenz@gmx.de>
2012-12-02 16:14:42 -0500

    adding my git plugin

---
+++ plugins/my-git/my-git.plugin.zsh
@@ -0,0 +1,27 @@
+function current_branch() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo ${ref#refs/heads/}
+}
+
+function current_repository() {
+
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo $(git remote -v | cut -d':' -f 2)
+}
+
+alias ga='git add'
+alias gp='git push'
+alias gl='git log'
+alias glv="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gs='git status --ignore-submodules -sb'
+alias gsv='git status --ignore-submodules'
+alias gd='git diff'
+alias gdc='git diff --cached'
+alias gm='git commit -m'
+alias gma='git commit -am'
+alias gb='git branch'
+alias gc='git checkout'
+alias gra='git remote add'
+alias grr='git remote rm'
+alias gpu='git pull --rebase'
+alias gcl='git clone'
