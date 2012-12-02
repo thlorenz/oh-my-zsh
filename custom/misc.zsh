240bca19368834552d56a078fee4764fb1cbfff8
Thorsten Lorenz <thlorenz@gmx.de>
2012-12-02 15:20:55 -0500

    adding custom path exports

--- custom/misc.zsh
+++ custom/misc.zsh
@@ -3,8 +3,3 @@ export EDITOR=vim
 
 # dircolors 
 export CLICOLOR=1
-
-# Landscape
-
-export NODE_PATH="/Users/thlorenz/dev/lab49/Landscape/trunk/packages"
-
---
+++ custom/paths.zsh
@@ -0,0 +1,4 @@
+export JS_PROJECTS="$HOME/dev/js/projects"
+
+# Landscape
+export NODE_PATH="/Users/thlorenz/dev/lab49/Landscape/trunk/packages"
